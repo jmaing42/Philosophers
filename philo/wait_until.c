b4cae28550fd164912c365297f7d94089cf67537
맹주영 <mjy9088@naver.com>
2022-09-09 16:48:27 +0900

    enhance wait

--- philo/wait_until.c
+++ philo/wait_until.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 13:04:06 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/09/09 15:46:24 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/09/09 15:57:45 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ t_err	wait_until(struct timeval until)
 {
 	struct timeval	now;
 
-	return (
-		false
-		|| gettimeofday(&now, NULL)
-		|| (is_bigger_than(now, until) && wrap_usleep(z(sub(until, now))))
-	);
+	if (gettimeofday(&now, NULL))
+		return (true);
+	while (is_bigger_than(now, until))
+		if (wrap_usleep(z(sub(until, now)) * 8 / 10))
+			return (true);
+	return (false);
 }
